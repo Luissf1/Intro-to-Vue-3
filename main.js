ed5f27c77b58f142e3c07a47dfb8a470f0cb3fbc
luissilvasf <luis.silva1252@gmail.com>
2023-11-30 18:12:09 -0800

    update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
+<<<<<<< Updated upstream
             inventory: 100,
             // solution
             onSale: true,
@@ -15,6 +16,29 @@ const app = Vue.createApp({
                 {id: 2234, color:'green'},
                 {id: 2235, color:'blue'}
             ],
+=======
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        // solution
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        // solution
+        updateImage(variantImage) {
+            this.image = variantImage
+>>>>>>> Stashed changes
         }
     }
 })
